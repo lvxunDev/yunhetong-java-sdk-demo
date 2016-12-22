f27fc1e4412e8e27a1ab101b7333cfa3ab16f387
SeanWu <seanwu@yunhetong.net>
2016-12-22 11:07:27 +0800

    添加了更新用户信息、查询合同列表、作废合同的Demo

---
+++ src/main/java/com/self/servlet/QueryContractsServlet.java
@@ -0,0 +1,52 @@
+package com.self.servlet;
+
+import com.yunhetong.sdk.LxSDKManager;
+import com.yunhetong.sdk.exception.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.self.resource.R.getLxSDKManager;
+
+
+public class QueryContractsServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        LxSDKManager lxSDKManager = getLxSDKManager();
+        String s = "";
+        try {
+            s = lxSDKManager.queryContracts(0,10);
+        } catch (LxEncryptException e) {
+            e.printStackTrace();
+        } catch (LxKeyException e) {
+            e.printStackTrace();
+        } catch (LxNonsupportException e) {
+            e.printStackTrace();
+        } catch (LxDecryptException e) {
+            e.printStackTrace();
+        } catch (LxSignatureException e) {
+            e.printStackTrace();
+        } catch (LxVerifyException e) {
+            e.printStackTrace();
+        }
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+
+        try {
+            response.getWriter().write(s.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+}
---
+++ src/main/java/com/self/servlet/UpdateUserInfoServlet.java
@@ -0,0 +1,51 @@
+package com.self.servlet;
+
+import com.self.resource.R;
+import com.yunhetong.sdk.LxSDKManager;
+import com.yunhetong.sdk.exception.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.self.resource.R.getLxSDKManager;
+
+
+public class UpdateUserInfoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+
+        LxSDKManager lxSDKManager = getLxSDKManager();
+
+        try {
+            String s = lxSDKManager.updateUserInfo(R.getUserA());
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json; charset=utf-8");
+            try {
+                response.getWriter().write(s.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }catch (LxEncryptException e) {
+            e.printStackTrace();
+        } catch (LxKeyException e) {
+            e.printStackTrace();
+        } catch (LxNonsupportException e) {
+            e.printStackTrace();
+        } catch (LxDecryptException e) {
+            e.printStackTrace();
+        } catch (LxSignatureException e) {
+            e.printStackTrace();
+        } catch (LxVerifyException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+}
